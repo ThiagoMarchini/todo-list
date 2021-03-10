481ad7b9d671aefb6ea385f653848d87eafbe1b5
Thiago Marchini <thiago@msn.com>
2021-03-10 15:52:54 -0300

    implementado o background cinza da tarefa selecionada e a seleçao de apenas uma tarefa

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ function criarTarefa() {
   const orderedList = document.getElementById('lista-tarefas');
   const newTask = document.createElement('li');
   newTask.innerHTML = task;
+  newTask.className = 'tarefa';
   orderedList.appendChild(newTask);
   document.getElementById('texto-tarefa').value = '';
+}
+
+function selectTask(task) {
+  const selectedTask = task;
+  if (selectedTask.className === 'tarefa') {
+    if (document.querySelector('.selected')) {
+      const oldSelection = document.querySelector('.selected');
+      oldSelection.className = 'tarefa';
+    }
+    selectedTask.className = 'tarefa selected';
+  } else {
+    selectedTask.className = 'tarefa';
+  }
 }
 
 // Captura de cliques e lógica básica
@@ -13,6 +27,9 @@ window.onload = function () {
   document.addEventListener('click', (event) => {
     if (event.target.id === 'criar-tarefa') {
       criarTarefa();
+    }
+    if (event.target.classList.contains('tarefa')) {
+      selectTask(event.target);
     }
   }, false);
 };
